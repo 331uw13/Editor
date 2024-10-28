2e494542315ef8702dc58a6bcdb01223893143e1
331uw13 <69628281+331uw13@users.noreply.github.com>
2024-10-28 21:47:42 +0200

    Update build.zig

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ const std = @import("std");
 pub fn build(b : *std.Build) !void {
 
     const exe = b.addExecutable(.{
-        .name = "idk",
+        .name = "editor",
         .target = b.standardTargetOptions(.{}),
     });
 
