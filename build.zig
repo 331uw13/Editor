87e77535ee4ebc62cbad8ebd30b6dfc9e1eba384
331uw13 <69628281+331uw13@users.noreply.github.com>
2024-10-28 21:42:05 +0200

    adding zig

---
+++ build.zig
@@ -0,0 +1,51 @@
+
+const std = @import("std");
+
+pub fn build(b : *std.Build) !void {
+
+    const exe = b.addExecutable(.{
+        .name = "idk",
+        .target = b.standardTargetOptions(.{}),
+    });
+
+    const flags = [_][]const u8{
+        "-Wall", "-Wextra", "-ggdb",
+        "-I/usr/include/freetype2",
+        "-I/usr/include/libpng16"
+    };
+
+
+    {
+        var dir = try std.fs.cwd().openDir("src", .{ .iterate = true });
+
+        var walker = try dir.walk(b.allocator);
+        defer walker.deinit();
+
+        while (try walker.next()) |entry| {
+            const ext = std.fs.path.extension(entry.basename);
+            var include_file = false;
+            if(std.mem.eql(u8, ext, ".c")) {
+                include_file = true;
+            }
+            if (include_file) {
+                exe.addCSourceFile(.{
+                    .file = b.path(b.pathJoin(&.{"src", entry.path})),
+                    .flags = &flags,
+                });
+            }
+        }
+    }
+
+    exe.linkSystemLibrary("c");
+    exe.linkSystemLibrary("glfw");
+    exe.linkSystemLibrary("GLEW");
+    exe.linkSystemLibrary("freetype");
+    exe.linkSystemLibrary("m");
+    b.default_step.dependOn(&exe.step);
+
+
+    b.installArtifact(exe);
+}
+
+
+
